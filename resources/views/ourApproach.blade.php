0690b5ede0885b8948006df597472bb587ff97b8
Usman Kapadiya <hirenk@intellidt.com>
2024-07-31 13:34:54 +0530

    container set

--- resources/views/ourApproach.blade.php
+++ resources/views/ourApproach.blade.php
@@ -12,47 +12,46 @@
 
 </head>
 <body>
-<div class="container-custom" id="page">
+    <div class="container" id="page">
         @include('header')
-        <div class="">
+    </div>
             <div>
-                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100" alt="Image 1">
+                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100 bannerHeight" alt="Image 1">
             </div>
-            <div class="container-custom mt-1 mb-5 p-4">
-            <h1 class="header-post-title-class">Our Approach</h1>
-                <div class="mt-3">
-                    
-                <p>
-                    Our investment process begins with identifying mispriced companies relative to their 
-                    intrinsic value. The investment team internally generates research ideas by 
-                    identifying high quality companies through meetings and screening 
-                    securities.&nbsp;Using our extensive local network we meet managements, 
-                    industry experts and assess&nbsp;a company’s ability to generate superior returns. 
-                    We look for high quality managements who know how to run their companies well, and 
-                    understand how to generate a cash return on capital above their cost of capital. 
-                    We look for businesses which demonstrate strong competitive advantages through 
-                    high barriers to entry, including technological, cost, brand or 
-                    distribution&nbsp;advantages. Finally, we&nbsp;like to buy companies at a discount 
-                    and we aim to invest in companies&nbsp;where we can see 50% upside to their 
-                    intrinsic value. Analysts make written and oral recommendations which the 
-                    portfolio manager weighs based on the level of conviction and risk to the 
-                    overall portfolio. The investment team continues to monitor progress in 
-                    portfolio companies while the portfolio manager in conjunction with the 
-                    risk manager monitors overall portfolio risk.
-                </p>
-                <p class="text-center mt-5 mb-5">
-                    <img 
-                        decoding="async" 
-                        class="aligncenter size-full wp-image-2037" 
-                        src="https://www.redwoodpeak.com/wp-content/uploads/2015/04/risk_management.png" 
-                        alt="investment-approach">
-                </p>
+            <div class="container">
+                <div class="container-custom mt-1 mb-5 p-4">
+                    <h1 class="header-post-title-class">Our Approach</h1>
+                    <div class="mt-3">                    
+                        <p>
+                            Our investment process begins with identifying mispriced companies relative to their 
+                            intrinsic value. The investment team internally generates research ideas by 
+                            identifying high quality companies through meetings and screening 
+                            securities.&nbsp;Using our extensive local network we meet managements, 
+                            industry experts and assess&nbsp;a company’s ability to generate superior returns. 
+                            We look for high quality managements who know how to run their companies well, and 
+                            understand how to generate a cash return on capital above their cost of capital. 
+                            We look for businesses which demonstrate strong competitive advantages through 
+                            high barriers to entry, including technological, cost, brand or 
+                            distribution&nbsp;advantages. Finally, we&nbsp;like to buy companies at a discount 
+                            and we aim to invest in companies&nbsp;where we can see 50% upside to their 
+                            intrinsic value. Analysts make written and oral recommendations which the 
+                            portfolio manager weighs based on the level of conviction and risk to the 
+                            overall portfolio. The investment team continues to monitor progress in 
+                            portfolio companies while the portfolio manager in conjunction with the 
+                            risk manager monitors overall portfolio risk.
+                        </p>
+                        <p class="text-center mt-5 mb-5">
+                            <img 
+                                decoding="async" 
+                                class="aligncenter size-full wp-image-2037" 
+                                src="https://www.redwoodpeak.com/wp-content/uploads/2015/04/risk_management.png" 
+                                alt="investment-approach">
+                        </p>
+                    </div>
                 </div>
             </div>
-
-        </div>
         @include('footer')
-    </div>
+    
    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>

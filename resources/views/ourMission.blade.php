d8d3614756e49deef9eba3e95a0a45e521d51152
Usman Kapadiya <hirenk@intellidt.com>
2024-07-31 11:59:41 +0530

    container set

--- resources/views/ourMission.blade.php
+++ resources/views/ourMission.blade.php
@@ -14,49 +14,48 @@
     </style>
 </head>
 <body>
-    <div class="container-custom" id="page">
+    <div class="container" id="page">
         @include('header')
-        <div >
-            <div>
-                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100" alt="Image 1">
-            </div>
-            <div class="container-custom mt-1 mb-5 p-4">
-                <h1 class="header-post-title-class">Our Mission</h1>
-                <div class="mt-3">
-                    <h3 class="fancytitle">
-                        <em>“Our
-                            mission is to achieve investment excellence for our clients through a </em><br>
-                        <em>results-oriented partnership between our clients, employees, and constituents.”</em>
-                    </h3>
-                    <p style="">
-                        Redwood Peak is run for the benefit of its investors and employees whose objectives are
-                        completely aligned. Profit without performance, size for its own sake, socially irresponsible
-                        investing, and the unilateral prosperity of employees without investors are to be rejected. Our
-                        firm should be successful if we work hard to attain investment excellence and integrity while
-                        placing the interests of our clients first.
-                    </p>
-                    <p style="">
-                        <span style="color: #823535;">To serve the clients</span><br>
-                        • To align our success with the objectives and success of our client<br>
-                        • To place the interests of our clients first
-                    </p>
-                    <p style="">
-                        <span style="color: #823535;">Respect for the individual</span><br>
-                        • To deal fairly and honestly with our business partners and associates<br>
-                        • To communicate openly and to be transparent with our partners and clients and employees
-                    </p>
-                    <p style="">
-                        <span style="color: #823535;">To strive for excellence</span><br>
-                        • To measure performance contribution<br>
-                        • To be a good corporate citizen by contributing to the community and making socially
-                        responsible investments
-                    </p>
+    </div>
+    <div>
+        <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100  bannerHeight" alt="Image 1" >
+    </div>
+            <div class="container">
+                <div class="container-custom mt-1 mb-5 p-4">
+                    <h1 class="header-post-title-class">Our Mission</h1>
+                    <div class="mt-3">
+                        <h3 class="fancytitle">
+                            <em>“Our
+                                mission is to achieve investment excellence for our clients through a </em><br>
+                            <em>results-oriented partnership between our clients, employees, and constituents.”</em>
+                        </h3>
+                        <p style="">
+                            Redwood Peak is run for the benefit of its investors and employees whose objectives are
+                            completely aligned. Profit without performance, size for its own sake, socially irresponsible
+                            investing, and the unilateral prosperity of employees without investors are to be rejected. Our
+                            firm should be successful if we work hard to attain investment excellence and integrity while
+                            placing the interests of our clients first.
+                        </p>
+                        <p style="">
+                            <span style="color: #823535;">To serve the clients</span><br>
+                            • To align our success with the objectives and success of our client<br>
+                            • To place the interests of our clients first
+                        </p>
+                        <p style="">
+                            <span style="color: #823535;">Respect for the individual</span><br>
+                            • To deal fairly and honestly with our business partners and associates<br>
+                            • To communicate openly and to be transparent with our partners and clients and employees
+                        </p>
+                        <p style="">
+                            <span style="color: #823535;">To strive for excellence</span><br>
+                            • To measure performance contribution<br>
+                            • To be a good corporate citizen by contributing to the community and making socially
+                            responsible investments
+                        </p>
+                    </div>
                 </div>
             </div>
-
-        </div>
         @include('footer')
-    </div>
    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
--- resources/views/overView.blade.php
+++ resources/views/overView.blade.php
@@ -12,32 +12,32 @@
 
 </head>
 <body>
-<div class="container-custom" id="page">
+<div class="container" id="page">
     @include('header')
+</div>
 
         <!-- New Row of Three Columns -->
-        <div class="">
             <div>
-                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100" alt="Image 1">
+                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100 bannerHeight" alt="Image 1">
             </div>
-            <div class="container-custom mt-1 mb-5 p-4">
-                <h1 class="header-post-title-class">Overview</h1>
-                <div class="mt-3">
-                    <p style="">
-                        Redwood Peak is a Hong Kong-based asset manager focused on fund management and separate account management for high net worth individuals, family offices, and institutions. Redwood Peak Limited was founded in 2007 as the Chilton Investment Company (HK) Limited and was spun off in a management buyout on 1st July 2014. Redwood Peak is regulated by the Securities and Futures Commission of Hong Kong (Type 4 Advising on securities and Type 9 Asset Management license). We invest capital for clients through a Hedge Fund and Managed Account platforms.
-                    </p>
-                    <p style="">
-                            Our investment process begins with identifying mispriced companies relative to their intrinsic value. In addition, we evaluate the quality and durability of a company’s business, the track record of a management team, and the underlying industry and macroeconomic trends. Redwood Peak’s investment edge is our extensive local network of relationships built over 45 years of combined investment experience. Our approach to portfolio construction involves risk assessment balanced with portfolio concentration versus diversification.
-                    </p>
-                    <h3 class="fancytitle">
-                        <em>“Our paramount objective is to serve our client’s investment goals.”</em>
-                    </h3>
+            <div class="container">
+
+                <div class="container-custom mt-1 mb-5 p-4">
+                    <h1 class="header-post-title-class">Overview</h1>
+                    <div class="mt-3">
+                        <p style="">
+                            Redwood Peak is a Hong Kong-based asset manager focused on fund management and separate account management for high net worth individuals, family offices, and institutions. Redwood Peak Limited was founded in 2007 as the Chilton Investment Company (HK) Limited and was spun off in a management buyout on 1st July 2014. Redwood Peak is regulated by the Securities and Futures Commission of Hong Kong (Type 4 Advising on securities and Type 9 Asset Management license). We invest capital for clients through a Hedge Fund and Managed Account platforms.
+                        </p>
+                        <p style="">
+                                Our investment process begins with identifying mispriced companies relative to their intrinsic value. In addition, we evaluate the quality and durability of a company’s business, the track record of a management team, and the underlying industry and macroeconomic trends. Redwood Peak’s investment edge is our extensive local network of relationships built over 45 years of combined investment experience. Our approach to portfolio construction involves risk assessment balanced with portfolio concentration versus diversification.
+                        </p>
+                        <h3 class="fancytitle">
+                            <em>“Our paramount objective is to serve our client’s investment goals.”</em>
+                        </h3>
+                    </div>
                 </div>
             </div>
-
-        </div>
         @include('footer')
-    </div>
    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>

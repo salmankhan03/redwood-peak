3d529f8bcae1ed256b28262b4aa8bda07f1e8d27
Usman Kapadiya <hirenk@intellidt.com>
2024-07-31 12:09:24 +0530

    container set

--- resources/views/hedgeFund.blade.php
+++ resources/views/hedgeFund.blade.php
@@ -12,54 +12,51 @@
 
 </head>
 <body>
-<div class="container-custom" id="page">
+    <div class="container" id="page">
         @include('header')
-        <div class="">
+    </div>
             <div>
-                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100" alt="Image 1">
+                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100 bannerHeight" alt="Image 1">
             </div>
-            <div class="container-custom mt-1 mb-5 p-4">
-            <h1 class="header-post-title-class">Hedge Fund</h1>
-                <div class="mt-3">
-                    
-                <p>
-                    Redwood Peak Opportunities Fund (the “Fund”)* is an&nbsp;Equity, Long-Short Fund launched 
-                    in January 2007, formerly known as the Chilton China Opportunities Fund.&nbsp;&nbsp;The 
-                    Fund aims to produce superior investment returns throughout various market cycles by 
-                    investing in companies located in or with exposure to the China region.&nbsp; Redwood 
-                    Peak seeks capital appreciation during favorable market&nbsp;conditions while preserving 
-                    capital in times of financial duress.&nbsp; The Fund uses a value approach and has a net 
-                    long bias, but utilizes shorts for both alpha generation and hedging purposes.
-                </p>
-                <p>
-                    The Fund’s research process is based upon bottom-up, fundamental analysis that typically 
-                    targets companies with four main characteristics: high-quality management teams, strong 
-                    business franchises, high barriers to entry through brand, scale or technology, and 
-                    reasonable valuations. The Fund primarily invests in publicly traded equities but has 
-                    the flexibility to invest in derivatives and other securities.
-                </p>
-                <p>
-                    The Fund is only available to institutions, family offices and high net worth individuals who 
-                    qualify as Professional Investors as defined by the Securities and Futures&nbsp;Commission.
-                </p>
-                <p>
-                    <em>
-                        * Please 
-                        <a style="text-decoration: none; padding: 0; color: #823535; margin: 0;" 
-                            href="../../contact-us/" 
-                            target="self">
-                            contact us
-                        </a> 
-                        or register for further details on the fund.
-                    </em>
-                </p>
+            <div class="container">
+                <div class="container-custom mt-1 mb-5 p-4">
+                    <h1 class="header-post-title-class">Hedge Fund</h1>
+                    <div class="mt-3">                    
+                        <p>
+                            Redwood Peak Opportunities Fund (the “Fund”)* is an&nbsp;Equity, Long-Short Fund launched 
+                            in January 2007, formerly known as the Chilton China Opportunities Fund.&nbsp;&nbsp;The 
+                            Fund aims to produce superior investment returns throughout various market cycles by 
+                            investing in companies located in or with exposure to the China region.&nbsp; Redwood 
+                            Peak seeks capital appreciation during favorable market&nbsp;conditions while preserving 
+                            capital in times of financial duress.&nbsp; The Fund uses a value approach and has a net 
+                            long bias, but utilizes shorts for both alpha generation and hedging purposes.
+                        </p>
+                        <p>
+                            The Fund’s research process is based upon bottom-up, fundamental analysis that typically 
+                            targets companies with four main characteristics: high-quality management teams, strong 
+                            business franchises, high barriers to entry through brand, scale or technology, and 
+                            reasonable valuations. The Fund primarily invests in publicly traded equities but has 
+                            the flexibility to invest in derivatives and other securities.
+                        </p>
+                        <p>
+                            The Fund is only available to institutions, family offices and high net worth individuals who 
+                            qualify as Professional Investors as defined by the Securities and Futures&nbsp;Commission.
+                        </p>
+                        <p>
+                            <em>
+                                * Please 
+                                <a style="text-decoration: none; padding: 0; color: #823535; margin: 0;" 
+                                    href="../../contact-us/" 
+                                    target="self">
+                                    contact us
+                                </a> 
+                                or register for further details on the fund.
+                            </em>
+                        </p>
+                    </div>
                 </div>
             </div>
-
-        </div>
-        @include('footer')
-    </div>
-   
+        @include('footer')   
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
--- resources/views/managedAccount.blade.php
+++ resources/views/managedAccount.blade.php
@@ -12,55 +12,54 @@
 
 </head>
 <body>
-<div class="container-custom" id="page">
+    <div class="container" id="page">
         @include('header')
-        <div class="">
-            <div>
-                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100" alt="Image 1">
-            </div>
-            <div class="container-custom mt-1 mb-5 p-4">
-            <h1 class="header-post-title-class">Managed Account</h1>
-                <div class="mt-3">
-                    
-                <p>
-                    Leveraging on our research and investment capabilities, we provide managed 
-                    account services. We partner with our clients and customize the investment 
-                    strategy to achieve their unique investment goals. Our platform allows 
-                    us to offer high quality and tailored investment and advisory services. 
-                    Investors have engaged us to manage a combination of these portfolios:
-                </p>
-                <ol>
-                    <li>
-                        China Portfolio – A concentrated China-focused portfolio of up to 15 
-                        high-quality companies which have long-term capital appreciation potential.
-                    </li>
-                    <li>
-                        Sequoia Portfolio – A concentrated global portfolio of up to 25 high-quality 
-                        companies which have long-term capital appreciation potential.
-                    </li>
-                    <li>
-                        Sprout Portfolio – A&nbsp;diversified global portfolio of up to 30 high 
-                        return small cap companies which have the potential for capital appreciation.
-                    </li>
-                    <li>
-                        Offshore Dividend Income Portfolio – A diversified global portfolio of up 
-                        to 30 income producing, steady cash flow and dividend generating companies 
-                        providing an average gross yield (before taxes) of&nbsp;greater than 5%.
-                    </li>
-                </ol>
-                <p>
-                    <em>* Please&nbsp;
-                        <a style="text-decoration: none; padding: 0; color: #823535; margin: 0;" 
-                            href="../../contact-us/" target="self">contact us</a>
-                        &nbsp;or register for further details on the managed account.
-                    </em>
-                </p>
-                </div>
-            </div>
-
-        </div>
-        @include('footer')
     </div>
+            <div>
+                <img src="{{ asset('assets/images/about-slider.jpg') }}" class="w-100 bannerHeight" alt="Image 1">
+            </div>
+            <div class="container">
+                <div class="container-custom mt-1 mb-5 p-4">
+                    <h1 class="header-post-title-class">Managed Account</h1>
+                    <div class="mt-3">
+                        <p>
+                            Leveraging on our research and investment capabilities, we provide managed 
+                            account services. We partner with our clients and customize the investment 
+                            strategy to achieve their unique investment goals. Our platform allows 
+                            us to offer high quality and tailored investment and advisory services. 
+                            Investors have engaged us to manage a combination of these portfolios:
+                        </p>
+                        <ol>
+                            <li>
+                                China Portfolio – A concentrated China-focused portfolio of up to 15 
+                                high-quality companies which have long-term capital appreciation potential.
+                            </li>
+                            <li>
+                                Sequoia Portfolio – A concentrated global portfolio of up to 25 high-quality 
+                                companies which have long-term capital appreciation potential.
+                            </li>
+                            <li>
+                                Sprout Portfolio – A&nbsp;diversified global portfolio of up to 30 high 
+                                return small cap companies which have the potential for capital appreciation.
+                            </li>
+                            <li>
+                                Offshore Dividend Income Portfolio – A diversified global portfolio of up 
+                                to 30 income producing, steady cash flow and dividend generating companies 
+                                providing an average gross yield (before taxes) of&nbsp;greater than 5%.
+                            </li>
+                        </ol>
+                        <p>
+                            <em>* Please&nbsp;
+                                <a style="text-decoration: none; padding: 0; color: #823535; margin: 0;" 
+                                    href="../../contact-us/" target="self">contact us</a>
+                                &nbsp;or register for further details on the managed account.
+                            </em>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        @include('footer')
    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>

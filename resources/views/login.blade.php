7b2e5042d632646641c15ac24f5c5332e5af1031
Usman Kapadiya <hirenk@intellidt.com>
2024-09-23 15:07:09 +0530

    login screen code update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,77 +16,74 @@
     </style>
 </head>
 <body>
-<div class="container" id="page">
-    @include('header')
-</div>
+    <div class="container" id="page">
+        @include('header')
+    </div>
 
-        <!-- New Row of Three Columns -->
-        <div class="container ">
-            <div class="container-custom mb-5 p-2 min-heights">
-                <!-- <h1 class="header-post-title-class static-position">Login</h1> -->
-                <div class="mt-4">
-                    <!-- Login Form -->
-                    <div class="mt-5 m-3">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+    <div class="container">
+        <div class="container-custom mb-5 p-2 min-heights">
+            <div class="mt-4">
+                <!-- Login Form -->
+                <div class="mt-5 m-3">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-                            <div class="row mt-4">
-                                <div class="col-md-12">
-                                    <label for="email">{{ __('E-mail') }}</label> <br/>
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                        @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                </div>
-                                <div class="col-md-12">
-                                    <label for="password">{{ __('Password') }}</label>
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                 </div>                              
-                            </div>
-                            <div class="form-group form-check mt-5 mb-1">
-                                <input type="checkbox" class="form-check-input w-auto" id="remember" name="remember">
-                                <label class="form-check-label" for="remember">{{ __('Keep me signed in') }}</label>
+                        <div class="row mt-4">
+                            <div class="col-md-12">
+                                <label for="email">{{ __('E-mail') }}</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
 
+                            <div class="col-md-12">
+                                <label for="password">{{ __('Password') }}</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
-                   
-                    <!-- Login Form -->
-                    
-                    <div class="form-group row mb-0 mt-2 ml-0 mr-0">
-                        <div class="">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Login') }}
-                            </button>
+                        <div class="form-group form-check mt-5 mb-1">
+                            <input type="checkbox" class="form-check-input" id="remember" name="remember">
+                            <label class="form-check-label" for="remember">{{ __('Keep me signed in') }}</label>
                         </div>
-                        <div class="ml-3">
-                            <button type="button" class="btn btn-secondary" onclick="redirectToRegister()" >
-                                {{ __('Register') }}
-                            </button>
+
+                        <div class="form-group row mb-0 mt-2">
+                            <div class="">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+                            </div>
+                            <div class="ml-3">
+                                <button type="button" class="btn btn-secondary" onclick="redirectToRegister()">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
                         </div>
-                    </div>
                     </form>
-                    </div>
                 </div>
-
             </div>
         </div>
-        @include('footer')
-    
+    </div>
+
+    @include('footer')
+
     <script>
-       function redirectToRegister() {
+        function redirectToRegister() {
             window.location.href = "{{ route('register') }}";
         }
     </script>
-   
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>

c2f86ea17c0b4829566623dbe937083743317129
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-11-27 15:14:53 +0530

    api update

--- database/migrations/2024_11_27_094217_change_column_type_in_users_table.php
+++ database/migrations/2024_11_27_094217_change_column_type_in_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('pages', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->text('status')->nullable()->change();
             $table->bigInteger('role_id')->nullable()->change();
 
@@ -23,7 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('pages', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->boolean('status')->nullable()->change();
             $table->boolean('role_id')->nullable()->change();
 

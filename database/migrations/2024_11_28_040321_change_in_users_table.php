961cf1c297767f5ee87f8461854dcc316f4457da
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-11-28 09:35:48 +0530

    migration added

---
+++ database/migrations/2024_11_28_040321_change_in_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('name')->nullable()->change();
+
+            $table->boolean('is_disabled')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('name')->change();
+
+            $table->dropColumn('is_disabled');
+
+
+        });
+    }
+};

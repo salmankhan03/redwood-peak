da4b8d837d360189b68ceac16f40bf0a2da86077
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-11-29 23:22:45 +0530

    api update

--- routes/api.php
+++ routes/api.php
@@ -47,7 +47,7 @@ Route::prefix('/post')->group(function () {
     Route::get('/{id}/delete', [PostController::class, 'delete']);
     Route::post('/multiple-delete', [PostController::class, 'multipleDelete']);
     Route::get('/{id}/get-by-id', [PostController::class, 'getById']);
-    Route::get('/edit', [PostController::class, 'edit']);
+    Route::post('/edit', [PostController::class, 'edit']);
 
    
 });
